2930fef3c501a0a87c01152098eff19f872223a7
tyler <tnegro@gmail.com>
2020-11-01 16:38:08 -0700

    user follows url builder

--- src/views/hooks/useTwitchUsers.js
+++ src/views/hooks/useTwitchUsers.js
@@ -2,16 +2,21 @@ import { useState, useEffect } from 'react'
 import { TWITCH_USERS_FOLLOWS, T_TKN, TWITCH_CLIENT_ID } from '../../consts'
 import { getApi} from '../../fetchUtil'
 
+function buildFollowsUrl({after, from_id, first}) {
+  return `${TWITCH_USERS_FOLLOWS}?from_id=${from_id}${after ? `&after=${after}` : ''}${first ? `&first=${first}` : ''}`
+}
 
-const useTwitchUsers = ({userId}) => {
+const useTwitchUsers = ({userId, cursor, first=20}) => {
 
   const [userFollows, setUserFollows] = useState()
+  console.log(buildFollowsUrl({from_id: userId, after: cursor, first}))
   useEffect(() => {
     if(userId){
       chrome.storage.local.get([T_TKN], (response) => {
         if(response[T_TKN]) {
           getApi({
-            url: TWITCH_USERS_FOLLOWS(userId),
+            
+            url: buildFollowsUrl({from_id: userId, after: cursor, first}),
             accessToken: response[T_TKN],
           })
             .then(data => {
@@ -23,7 +28,7 @@ const useTwitchUsers = ({userId}) => {
       })
     }
 
-  }, [userId])
+  }, [userId, cursor])
 
   return {
     userFollows
